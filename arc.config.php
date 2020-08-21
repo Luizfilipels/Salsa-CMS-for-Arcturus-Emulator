f1bce9253c8c5c6c8e3b9c9ea9a18fe0c4e5fee4
Luiz <luizfilipesl2003@gmail.com>
2020-08-21 16:26:07 -0300

    Alterando para minhas configs

--- arc.config.php
+++ arc.config.php
@@ -1,14 +1,14 @@
 <?php
 // Configurações do banco de dados
 $config['database_hostname'] = "localhost"; // Hostname
-$config['database_username'] = "user"; // Nome de usuário do banco de dados
-$config['database_password'] = "pass"; // Senha do usuário do banco de dados
-$config['database_name'] = "db"; // Nome do banco de dados
+$config['database_username'] = "habbo"; // Nome de usuário do banco de dados
+$config['database_password'] = "password"; // Senha do usuário do banco de dados
+$config['database_name'] = "cosmic"; // Nome do banco de dados
 // Configurações do site
 
 
-$config['site.nome'] = "Habbo"; // Nome do Hotel
-$config['site.url'] = "http://localhost"; // URL do Hotel
+$config['site.nome'] = "Shitel"; // Nome do Hotel
+$config['site.url'] = "http://shitel.com.br"; // URL do Hotel
 $config['404page'] = "404"; // Nome da página de erro
 $config['site.manutencao'] = 0; // Colocar ou remover manutenção do hotel
 $config['site.login'] = 1; // Desativar ou ativar o login no Hotel
@@ -28,35 +28,35 @@ $config['painel.premiar_codigo_emblema'] = "NV"; // Código do emblema de nívei
 // Configurações do Registro
 $config['registro.ativado'] = 1; // Desativar ou ativar o registro do Hotel
 $config['registro.captcharegistro'] = 1; // Captcha ativada/desativada para  impedir BOTS
-$config['registro.missao'] = "Dominar o mundo"; // Missão inicial
-$config['registro.creditos'] = "100"; // Créditos iniciais
+$config['registro.missao'] = "Shitpostar ate o cu fazer bico"; // Missão inicial
+$config['registro.creditos'] = "1000"; // Créditos iniciais
 $config['registro.visual'] = "hr-115-45.hd-209-2.ch-255-76.lg-275-62.fa-1204.ca-1806-65"; // Visual inicial
 $config['registro.quartoinicial'] = "1"; // Quarto inicial
 $config['registro.contasporip'] = 50; // Quantidade de contas por IP
 
 
 // Configurações da Client
-$config['connection.info.host'] = "localhost"; // IP da VPS ou Hamachi
+$config['connection.info.host'] = "168.227.112.31"; // IP da VPS ou Hamachi
 $config['connection.info.port'] = "30000"; // Porta do emulador
-$config['url.prefix'] = "http://localhost";
+$config['url.prefix'] = "http://shitel.com.br";
 $config['client.starting'] = "Bem vindo!";
-$config['external.texts.txt'] = "http://localhost/swf/gamedata/external_flash_texts.txt";
-$config['furnidata.load.url'] = "http://localhost/swf/gamedata/furnidata.xml";
-$config['external.variables.txt'] = "http://localhost/swf/gamedata/external_variables.txt";
-$config['external.override.texts.txt'] = "http://localhost/swf/gamedata/override/external_flash_override_texts.txt";
-$config['external.figurepartlist.txt'] = "http://localhost/swf/gamedata/figuredata.xml";
-$config['productdata.load.url'] = "http://localhost/swf/gamedata/productdata.txt";
-$config['external.override.variables.txt'] = "http://localhost/swf/gamedata/override/external_override_variables.txt";
-$config['flash.client.url'] = "http://localhost//swf/gordon/PRODUCTION-201601012205-226667486/";
-$config['habbo.swf'] = "http://localhost/swf/gordon/PRODUCTION-201601012205-226667486/Habbo.swf";
+$config['external.texts.txt'] = "http://images.shitel.com.br/swf/gamedata/external_flash_texts.txt";
+$config['furnidata.load.url'] = "http://images.shitel.com.br/swf/gamedata/furnidata.xml";
+$config['external.variables.txt'] = "http://images.shitel.com.br/swf/gamedata/external_variables.txt";
+$config['external.override.texts.txt'] = "http://images.shitel.com.br/swf/gamedata/override/external_flash_override_texts.txt";
+$config['external.figurepartlist.txt'] = "http://images.shitel.com.br/swf/gamedata/figuredata.xml";
+$config['productdata.load.url'] = "http://images.shitel.com.br/swf/gamedata/productdata.txt";
+$config['external.override.variables.txt'] = "http://images.shitel.com.br/swf/gamedata/override/external_override_variables.txt";
+$config['flash.client.url'] = "http://images.shitel.com.br//swf/gordon/PRODUCTION-202006192205-424220153/";
+$config['habbo.swf'] = "http://images.shitel.com.br/swf/gordon/PRODUCTION-202006192205-424220153/Habbo.swf";
 
 
 // Configurações do tema atual
 $config['tema.tema'] = "habbo";
 $config['tema.logo'] = "http://habbox.com/text/70/Habbo";
 $config['tema.topheader'] = "https://i.imgur.com/tgTqRgb.gif";
-$config['tema.urlemblemas'] = "http://images.habbo.com/c_images/album1584/"; // Pasta de emblemas do perfil
+$config['tema.urlemblemas'] = "http://images.shitel.com.br/c_images/album1584/"; // Pasta de emblemas do perfil
 $config['tema.postagens'] = 1; // Desativar ou ativar as postagens na página principal
 $config['tema.facebook'] = "http://facebook.com/Habbo";
 $config['tema.twitter'] = "http://twitter.com/Habbo";
-$config['tema.discord'] = "http://discord.gg/Habbo";
+$config['tema.discord'] = "https://discord.gg/xPQrKMM";
